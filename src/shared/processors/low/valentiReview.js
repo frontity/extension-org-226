de6ced300e9135f7d630573f74b33e1006a52a3e
David Arenas <darensr@gmail.com>
2019-02-04 17:43:55 +0100

    feat(valenti-reviews): create processor

--- src/shared/processors/low/valentiReview.js
+++ src/shared/processors/low/valentiReview.js
@@ -1,22 +1,57 @@
 import ValentiReview from '../../components/ValentiReview';
 
-const findElement = (element, className) => {
-  const { props, children } = element;
-  if (props && props.className && props.className.includes(className)) {
-    return element;
-  }
-  return children && children.find(e => findElement(e, className));
+const hasClass = (e, c) => {
+  const { props = {} } = e;
+  const { className } = props;
+  return !!(className && className.includes(c));
 };
 
-const getTitle = element => {
-  const titleElement = findElement(element, 'cb-review-title');
-  let title = '';
+const find = (element, className) => {
+  if (hasClass(element, className)) return element;
+  return (
+    element.children &&
+    element.children.reduce((found, e) => found || find(e, className), null)
+  );
+};
 
-  if (titleElement) {
-    [{ content: title }] = titleElement.children;
-  }
+export const getTitle = e => find(e, 'cb-review-title').children[0].content;
+export const getLabel = e => e.children[0].content;
+export const getStars = e => e.children[0].props.style.match(/(\d+%)/)[1];
 
-  return title;
+export const getSummary = e => {
+  const [scoreElement, labelElement, starsElement] = find(
+    e,
+    'cb-score-box',
+  ).children;
+  return {
+    score: scoreElement.children[0].content,
+    label: getLabel(labelElement),
+    stars: getStars(starsElement),
+  };
+};
+
+export const getCriteria = e => {
+  const { children: allCriteria } = find(e, 'cb-criteria-area');
+  return allCriteria.map(({ children }) => {
+    const [labelElement, starsElement] = children;
+    return {
+      label: getLabel(labelElement),
+      stars: getStars(starsElement),
+    };
+  });
+};
+
+export const getUserRating = e => {
+  const [labelElement, starsElement] = find(
+    e,
+    'cb-user-rating',
+  ).children[0].children;
+  const votesElement = labelElement.children[1];
+  return {
+    label: getLabel(labelElement),
+    votes: votesElement.children[0].content,
+    stars: getStars(starsElement),
+  };
 };
 
 export default {
@@ -27,23 +62,9 @@ export default {
   process: element => {
     const props = {
       title: getTitle(element),
-      summary: {
-        label: 'Nota Final',
-        score: '8.7',
-        stars: '13%',
-      },
-      criteria: [
-        { label: 'Precios', stars: '12%' },
-        { label: 'Métodos de pago', stars: '12%' },
-        { label: 'Diseño y usabilidad', stars: '28%' },
-        { label: 'Información al cliente', stars: '7%' },
-        { label: 'Servicios', stars: '6%' },
-      ],
-      userRating: {
-        label: 'Puntación de los lectores ',
-        votes: '0 Votos',
-        stars: '100%',
-      },
+      summary: getSummary(element),
+      criteria: getCriteria(element),
+      userRating: getUserRating(element),
     };
 
     return { component: ValentiReview, props };
