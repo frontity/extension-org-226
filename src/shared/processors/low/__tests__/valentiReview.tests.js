9dbece82f37e05dde39b6e2dcceda886a79604ba
David Arenas <darensr@gmail.com>
2019-02-04 17:44:33 +0100

    test(valenti-reviews): test processor

--- src/shared/processors/low/__tests__/valentiReview.tests.js
+++ src/shared/processors/low/__tests__/valentiReview.tests.js
@@ -1,3 +1,30 @@
+import Component from '../../../components/ValentiReview';
+import main, {
+  getTitle,
+  getSummary,
+  getCriteria,
+  getUserRating,
+} from '../valentiReview';
+import data from './data/valentiReview.json';
+
 describe('Extension Organization 226 › Shared › Processors › low › valentiReview', () => {
-  test('first test', () => {});
+  test('extracts title ', () => {
+    expect(getTitle(data)).toMatchSnapshot();
+  });
+  test('extracts summary data ', () => {
+    expect(getSummary(data)).toMatchSnapshot();
+  });
+  test('extracts all criteria ', () => {
+    expect(getCriteria(data)).toMatchSnapshot();
+  });
+  test('extracts user rating', () => {
+    expect(getUserRating(data)).toMatchSnapshot();
+  });
+  test('processor works as expected', () => {
+    const { test: t, process: p } = main;
+    expect(t(data)).toBeTruthy();
+    const processed = p(data);
+    expect(processed.component).toBe(Component);
+    expect(processed).toMatchSnapshot();
+  });
 });
